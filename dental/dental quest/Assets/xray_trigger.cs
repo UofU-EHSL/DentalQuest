98b774e9445aec63b286ac763f927fae9781545f
Ben <benjamin.engel@utah.edu>
2019-10-22 14:19:26 -0600

    Hey, at least it runs now

--- dental/dental quest/Assets/xray_trigger.cs
+++ dental/dental quest/Assets/xray_trigger.cs
@@ -9,27 +9,27 @@ namespace Valve.VR.InteractionSystem
     public class xray_trigger : MonoBehaviour
     {
         public bool clicked;
-        public Interactable interactable;
+        //public Interactable interactable;
         public rendure_texture_to_texture2d screenCapture;
-        [SteamVR_DefaultAction("Trigger")]
-        public SteamVR_Action_Boolean trigger;
+        //[SteamVR_DefaultAction("Trigger")]
+        //public SteamVR_Action_Boolean trigger;
 
         private void Start()
         {
-            if (interactable == null)
-                interactable = GetComponent<Interactable>();
+            //if (interactable == null)
+            //    interactable = GetComponent<Interactable>();
         }
         private void Update()
         {
-            if (interactable.attachedToHand)
-            {
-                clicked = trigger.GetState(interactable.attachedToHand.handType);
-                if (clicked == true)
-                {
-                    screenCapture.take();
-                    clicked = false;
-                }
-            }
+            //if (interactable.attachedToHand)
+            //{
+            //    clicked = trigger.GetState(interactable.attachedToHand.handType);
+            //    if (clicked == true)
+            //    {
+            //        screenCapture.take();
+            //        clicked = false;
+            //    }
+            //}
         }
     }
 }
