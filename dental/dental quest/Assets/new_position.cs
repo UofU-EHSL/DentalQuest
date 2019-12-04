4c74c8958a6fd980e44700c3be11ee6a4f336686
Ben <benjamin.engel@utah.edu>
2019-12-04 14:54:15 -0700

    update to add a number of scenes with different stuff in them

---
+++ dental/dental quest/Assets/new_position.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class new_position : MonoBehaviour
+{
+    public Vector3 usefulPosition;
+    public Vector3 outOfTheWay;
+    public GameObject thing;
+
+    public void useful()
+    {
+        thing.transform.localPosition = usefulPosition;
+    }
+    public void GetOut()
+    {
+        thing.transform.localPosition = outOfTheWay;
+    }
+}
---
+++ dental/dental quest/Assets/timer.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Events;
+
+public class timer : MonoBehaviour
+{
+
+    public Text text;
+    public string text_before;
+    public float time;
+    public int minutes;
+    public int seconds;
+    public GameObject[] show;
+    public GameObject[] hide;
+    public bool timing;
+    public UnityEvent WhenTimerEnds;
+    // Use this for initialization
+
+
+    void Update()
+    {
+        if (timing == true)
+        {
+            time -= Time.deltaTime;
+            minutes = Mathf.FloorToInt(time / 60);
+            seconds = Mathf.RoundToInt(time - (minutes * 60));
+            text.text = text_before + minutes.ToString() + "m " + seconds.ToString() + "s";
+            if (time <= 0)
+            {
+                GameOver();
+            }
+        }
+    }
+    public void timer_start()
+    {
+        time = 300;
+        timing = true;
+    }
+    void GameOver()
+    {
+        WhenTimerEnds.Invoke();
+        timing = false;
+        foreach (GameObject item in hide)
+        {
+            item.SetActive(false);
+        }
+        foreach (GameObject item in show)
+        {
+            item.SetActive(true);
+        }
+    }
+}
