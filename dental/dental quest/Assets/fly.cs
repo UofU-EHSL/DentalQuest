b282fe8d0927e222668ef4c2358225bd6b7c8369
Ben <benjamin.engel@utah.edu>
2020-01-13 14:57:57 -0700

    Flying for endo thigns

---
+++ dental/dental quest/Assets/fly.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Zinnia.Data.Type.Transformation.Conversion;
+
+public class fly : MonoBehaviour
+{
+    public GameObject tooth;
+    public FloatToVector2 controller;
+    public GameObject player;
+    public float speed;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        tooth.transform.position = tooth.transform.position + Camera.main.transform.forward * (controller.GetComponent<FloatToVector2>().CurrentY*speed) * Time.deltaTime;
+    }
+}
