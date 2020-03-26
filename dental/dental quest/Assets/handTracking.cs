89bc3edb96586e2bb59fc3a28af377c18cb52fc5
ben <benengel94@gmail.com>
2020-03-26 13:15:42 -0600

    oh so many changes. Hand tracking now works in the denture placement scene

---
+++ dental/dental quest/Assets/handTracking.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class trackedThing
+{
+    public GameObject fingerCollider;
+    public OVRSkeleton.BoneId singleBone;
+}
+
+public class handTracking : MonoBehaviour
+{
+    public OVRSkeleton skeleton;
+    public trackedThing[] bones;
+
+    void Update()
+    {
+        foreach (trackedThing single in bones)
+        {
+            foreach (OVRBone bone in skeleton.Bones)
+            {
+                if (bone.Id == single.singleBone)
+                {
+                    single.fingerCollider.transform.SetParent(bone.Transform);
+                    single.fingerCollider.transform.localPosition = new Vector3(0, 0, 0);
+                }
+            }
+        }
+    }
+}
---
+++ dental/dental quest/Assets/move_teeth.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class move_teeth : MonoBehaviour
+{
+    public GameObject[] moveObject;
+    public Text text;
+    public float value;
+    public float scale;
+
+    public void updateValue(float buttonValue)
+    {
+        value = value+buttonValue;
+        foreach (GameObject thing in moveObject)
+        {
+            thing.transform.localPosition = new Vector3(thing.transform.localPosition.x, value*scale, thing.transform.localPosition.z);
+        }
+        if (text) {
+            text.text = value.ToString("F2") + "mm";
+        }
+    }
+}
