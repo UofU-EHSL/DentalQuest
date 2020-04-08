7b6bfb7d78e4db676535d71c4ce0feaf8f91c5f3
BenEngel <benengel94@gmail.com>
2020-04-08 10:06:10 -0600

    changed a bunch of junk

---
+++ dental/dental quest/Assets/hand_grgab.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class hand_grgab : MonoBehaviour
+{
+    public GameObject hand;
+    public GameObject finger1;
+    public GameObject[] finger2;
+    public GameObject grab_object;
+    public Color fingerTouchouch;
+    public void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "Grabbable"){
+            grab_object = other.gameObject;
+        }
+    }
+    public void OnTriggerExit(Collider other)
+    {
+            if(other.gameObject == grab_object){
+            grab_object = null;    
+            }
+    }
+
+    public void Update()
+    {
+        foreach(GameObject thumb in finger2){
+            if (thumb.GetComponent<thumb_enter>().triggered == true)
+            {
+                grab_object.transform.SetParent(hand.transform);
+                thumb.GetComponent<MeshRenderer>().material.color = fingerTouchouch;
+                return;
+            }
+            else
+            {
+                grab_object.transform.parent = null;
+            }
+        }
+    }
+}
---
+++ dental/dental quest/Assets/thumb_enter.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class thumb_enter : MonoBehaviour
+{
+    public GameObject finger;
+    public bool triggered;
+
+    public void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject == finger){
+            triggered = true;
+            GetComponent<Renderer>().material.color = new Color(0.5f, 1, 1);
+        }
+    }
+    public void OnTriggerExit(Collider other)
+    {
+        if(other.gameObject == finger){
+            triggered = false;
+            GetComponent<Renderer>().material.color = new Color(1, 1, 1);
+        }
+    }
+}
